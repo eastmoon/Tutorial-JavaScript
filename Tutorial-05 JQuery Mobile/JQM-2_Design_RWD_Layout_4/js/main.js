0eb0f91b12c52d1fe67da1096fc1c53dc22f8349
Jacky Eastmoon <jacky_eastmoon@hotmail.com>
2015-12-11 14:37:59 +0800

    20151211
    JQM-2_Design_RWD_Layout_4 modify javascript file defined.

--- Tutorial-05 JQuery Mobile/JQM-2_Design_RWD_Layout_4/js/main.js
+++ Tutorial-05 JQuery Mobile/JQM-2_Design_RWD_Layout_4/js/main.js
@@ -3,39 +3,39 @@
  */
 $(document).ready(Initial);
 $(window).resize(Update);
-    function Initial()
-    {
-        // Bootstrap Reference : https://docs.angularjs.org/guide/bootstrap
-        // Initial Angular boostrap with current document.
-        //angular.bootstrap(document);
-        // Initial Angular with current document and module is myApp.
-        // angular.bootstrap( document, ["myApp"] );
+function Initial()
+{
+    // Bootstrap Reference : https://docs.angularjs.org/guide/bootstrap
+    // Initial Angular boostrap with current document.
+    //angular.bootstrap(document);
+    // Initial Angular with current document and module is myApp.
+    // angular.bootstrap( document, ["myApp"] );
 
-        // Initial interaction
-        console.info("Initial Application :");
+    // Initial interaction
+    console.log("Initial Application :");
 
-        // Clone element content to target element, target is attribute value.
-        console.log("Clone element.")
-        $("div[clone-by]").html( function() {
-            return $("#" + $(this).attr("clone-by")).html();
-        });
+    // Clone element content to target element, target is attribute value.
+    console.log("Clone element.")
+    $("div[clone-by]").html( function() {
+        return $("#" + $(this).attr("clone-by")).html();
+    });
 
-        // Start outside panel function
-        // reference : https://demos.jquerymobile.com/1.4.5/panel-external-internal/index.html
-        $( "body > [data-role='panel']" ).panel();
+    // Start outside panel function
+    // reference : https://demos.jquerymobile.com/1.4.5/panel-external-internal/index.html
+    $( "body > [data-role='panel']" ).panel();
 
-        // Fixed external toolbars
-        // reference : http://demos.jquerymobile.com/1.4.4/toolbar-external/
-        $(function () {
-            $("[data-role='header']").toolbar({ theme: "a" });
-            $("[data-role='footer']").toolbar({ theme: "a" });
-        });
+    // Fixed external toolbars
+    // reference : http://demos.jquerymobile.com/1.4.4/toolbar-external/
+    $(function () {
+        $("[data-role='header']").toolbar({ theme: "a" });
+        $("[data-role='footer']").toolbar({ theme: "a" });
+    });
 
-        // Initial View
-        console.log("update view.");
-        Update();
-    }
+    // Initial View
+    console.log("Update view.");
+    Update();
+}
 
-    function Update()
-    {
-    }
+function Update()
+{
+}
