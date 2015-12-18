a3556d8e137beebe8daa1cf271f9697ac5cae321
Jacky Eastmoon <jacky_eastmoon@hotmail.com>
2015-12-18 16:54:28 +0800

    20151218
    Custom defined all jQuery Mobile default setting.

---
+++ Tutorial-05 JQuery Mobile/application/jqm.custom.defined.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Chen on 2015/12/11.
+ */
+// Reference : http://demos.jquerymobile.com/1.1.2/docs/api/globalconfig.html
+// This custom defined file must load before jquery.mobile.js
+$(document).bind("mobileinit", function(){
+    $.mobile.buttonMarkup.hoverDelay = 0;
+    $.mobile.defaultPageTransition = "flip";
+    $.mobile.defaultPageTransition = "flip";
+});
